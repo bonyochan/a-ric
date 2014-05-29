157e2b0d6ef4e81c8e2f4cf10c1fa71c5473de5b
bonyochan <60gics@gmail.com>
2014-05-29 13:50:25 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<html>
+  <head>
+    <title>
+      anticruit
+    </title>
+  </head>
+  <body>
+    <h1>就活サイト</h1>
+    <iframe width="560" height="315" src="//www.youtube.com/embed/M6rb6kknj3A" frameborder="0" allowfullscreen></iframe>
+  </body>
+</html>
